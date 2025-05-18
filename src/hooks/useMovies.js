4de8cca0d1a6c99e6fa7032975398650326597df
MancillaO <edgarmncllav04@gmail.com>
2025-05-18 16:10:01 -0600

    no dejar que el usuario haga la misma busqueda dos veces

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { searchMovies } from '../services/movies.js'
 
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    if (previousSearch.current === search) return
     try {
       setLoading(true)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (error) {
